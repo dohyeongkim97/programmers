e416da0852e460061304d9c17e81c7c648215f6a
dohyeong <127936619+dohyeongkim97@users.noreply.github.com>
2023-10-10 01:07:45 +0900

    uploaded at 2023. 10. 10. 오전 1:07:41

---
+++ programmers/가격이 제일 비싼 식품의 정보 출력하기/solution.sql
@@ -0,0 +1,6 @@
+// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/131115
+
+-- 코드를 입력하세요
+SELECT PRODUCT_ID, PRODUCT_NAME, PRODUCT_CD, CATEGORY, PRICE from FOOD_PRODUCT
+order by PRICE desc
+limit 1;
