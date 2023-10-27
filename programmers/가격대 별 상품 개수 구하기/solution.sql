245aa4fb69fded9395c0dd5015081394d9a453a0
dohyeong <127936619+dohyeongkim97@users.noreply.github.com>
2023-10-28 00:19:21 +0900

    uploaded at 2023. 10. 28. 오전 12:19:23

---
+++ programmers/가격대 별 상품 개수 구하기/solution.sql
@@ -0,0 +1,6 @@
+// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/131530
+
+-- 코드를 입력하세요
+SELECT FLOOR(PRICE/10000)*10000 AS PRICE_GROUP, COUNT(PRODUCT_ID) AS PRODUCTS FROM PRODUCT
+GROUP BY PRICE DIV 10000
+ORDER BY PRICE
