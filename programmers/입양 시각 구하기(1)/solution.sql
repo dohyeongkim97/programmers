38daad559ec359ae1238f4c667251371a5406275
dohyeong <127936619+dohyeongkim97@users.noreply.github.com>
2023-10-28 00:12:04 +0900

    uploaded at 2023. 10. 28. 오전 12:12:05

---
+++ programmers/입양 시각 구하기(1)/solution.sql
@@ -0,0 +1,8 @@
+// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/59412
+
+-- 코드를 입력하세요
+SELECT HOUR(DATETIME) AS HOUR, COUNT(ANIMAL_ID) AS COUNT 
+FROM ANIMAL_OUTS 
+WHERE HOUR(DATETIME) BETWEEN 9 AND 19
+GROUP BY HOUR(DATETIME) 
+ORDER BY HOUR(DATETIME)
