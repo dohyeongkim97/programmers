bc73da889df22fb6671fbd8e3f9854c1b5709bfb
dohyeong <127936619+dohyeongkim97@users.noreply.github.com>
2023-11-13 14:48:37 +0900

    uploaded at 2023. 11. 13. 오후 2:48:48

---
+++ programmers/없어진 기록 찾기/solution.sql
@@ -0,0 +1,5 @@
+// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/59042
+
+SELECT A.ANIMAL_ID, A.NAME FROM
+(ANIMAL_OUTS A LEFT JOIN ANIMAL_INS B ON A.ANIMAL_ID = B.ANIMAL_ID)
+WHERE INTAKE_CONDITION IS NULL;
