9deb86afa03bc316abefc787532cf7d15f8e7fb6
dohyeong <127936619+dohyeongkim97@users.noreply.github.com>
2023-11-21 21:46:44 +0900

    uploaded at 2023. 11. 21. 오후 9:46:44

---
+++ programmers/보호소에서 중성화한 동물/solution.sql
@@ -0,0 +1,6 @@
+// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/59045
+
+-- 코드를 입력하세요
+SELECT A.ANIMAL_ID, A.ANIMAL_TYPE, A.NAME FROM 
+ANIMAL_INS A LEFT JOIN ANIMAL_OUTS B ON A.ANIMAL_ID = B.ANIMAL_ID
+WHERE A.SEX_UPON_INTAKE <> B.SEX_UPON_OUTCOME;
