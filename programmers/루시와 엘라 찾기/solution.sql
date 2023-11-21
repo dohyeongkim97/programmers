5de65ecdb445827d22445ea9f3a0ab2364267da9
dohyeong <127936619+dohyeongkim97@users.noreply.github.com>
2023-11-21 21:40:45 +0900

    uploaded at 2023. 11. 21. 오후 9:40:45

---
+++ programmers/루시와 엘라 찾기/solution.sql
@@ -0,0 +1,11 @@
+// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/59046
+
+-- 코드를 입력하세요
+SELECT ANIMAL_ID, NAME, SEX_UPON_INTAKE from ANIMAL_INS 
+WHERE NAME LIKE 'LUCY' 
+OR NAME LIKE 'ELLA'
+OR NAME LIKE 'PICKLE'
+OR NAME LIKE 'ROGAN'
+OR NAME LIKE 'SABRINA'
+OR NAME LIKE 'MITTY'
+ORDER BY ANIMAL_ID;
