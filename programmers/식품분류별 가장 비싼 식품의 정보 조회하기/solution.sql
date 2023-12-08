5a2bdcdb10a434a3484a68a258a3189d33658e16
dohyeong <127936619+dohyeongkim97@users.noreply.github.com>
2023-12-08 21:22:35 +0900

    uploaded at 2023. 12. 8. 오후 9:22:38

---
+++ programmers/식품분류별 가장 비싼 식품의 정보 조회하기/solution.sql
@@ -0,0 +1,8 @@
+// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/131116
+
+-- 코드를 입력하세요
+SELECT CATEGORY, PRICE AS MAX_PRICE, PRODUCT_NAME
+FROM FOOD_PRODUCT
+WHERE CATEGORY IN ('과자', '국', '식용유', '김치')
+AND PRICE IN (SELECT MAX(PRICE) FROM FOOD_PRODUCT GROUP BY CATEGORY)
+ORDER BY MAX_PRICE DESC;
