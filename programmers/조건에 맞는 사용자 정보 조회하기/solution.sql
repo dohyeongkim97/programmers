d06c815322c339587a40fba9a7d3068f85fb517e
dohyeong <127936619+dohyeongkim97@users.noreply.github.com>
2023-12-08 21:14:34 +0900

    uploaded at 2023. 12. 8. 오후 9:14:36

---
+++ programmers/조건에 맞는 사용자 정보 조회하기/solution.sql
@@ -0,0 +1,16 @@
+// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/164670
+
+-- 코드를 입력하세요
+SELECT 
+    USER_ID, 
+    NICKNAME, 
+    CONCAT(B.CITY, ' ', B.STREET_ADDRESS1, ' ', B.STREET_ADDRESS2) AS '전체주소',
+    CONCAT(SUBSTRING(B.TLNO, 1, 3), '-',
+          SUBSTRING(B.TLNO, 4, 4), '-',
+          SUBSTRING(B. TLNO, 8)) AS '전화번호'
+FROM (SELECT * FROM USED_GOODS_BOARD
+      GROUP BY WRITER_ID
+      HAVING  COUNT(*)>=3)
+      A LEFT JOIN USED_GOODS_USER B
+ON A.WRITER_ID = B.USER_ID
+ORDER BY USER_ID DESC
