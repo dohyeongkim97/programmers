bcf08b621c6b89cf0a9a59eba9a88fb5add4f756
dohyeong <127936619+dohyeongkim97@users.noreply.github.com>
2024-02-05 07:43:49 +0900

    uploaded at 2024. 2. 5. 오전 7:43:45

---
+++ programmers/최대공약수와 최소공배수/solution.py
@@ -0,0 +1,10 @@
+// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/12940
+
+import math
+def solution(n, m):
+    answer = []
+    
+    answer.append(math.gcd(n, m))
+    answer.append((n*m)//math.gcd(n, m))
+    
+    return answer
