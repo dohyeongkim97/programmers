ba774df8c2231a01faf3ffc48f0827ab459974c8
dohyeong <127936619+dohyeongkim97@users.noreply.github.com>
2024-02-08 10:31:03 +0900

    [level 2] Title: 할인 행사, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.py
@@ -0,0 +1,18 @@
+def solution(want, number, discount):
+    answer = 0
+    want_dict = {}
+    for i in range(len(want)):
+        want_dict[want[i]] = number[i]
+    
+    for i in range(len(discount)-10+1):
+        day_dict = {}
+        for j in range(i, i+10):
+            if discount[j] in day_dict.keys():
+                day_dict[discount[j]] += 1
+            else:
+                day_dict[discount[j]] = 1
+            
+        if day_dict == want_dict:
+            answer += 1
+            
+    return answer
